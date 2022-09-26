3044f8b447a417796f5e0f59856158075a68a4c6
GoldyXDXD <62290862+GoldyXDXD@users.noreply.github.com>
2022-09-26 14:59:38 +0300

    Добавлены методы включения и выключения автозапуска

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,14 +6,31 @@ import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 import java.awt.*;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URISyntaxException;
+import java.util.Hashtable;
 
 public class Main {
     public static void main(String[] args) throws IOException, URISyntaxException {
+        /*
         Hashtable<EncodeHintType, ErrorCorrectionLevel> hintMap = new Hashtable<EncodeHintType, ErrorCorrectionLevel>();
         hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
 
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
+
         BitMatrix bitMatrix = qrCodeWriter.encode(content, BarcodeFormat.QR_CODE, qrCodeSize, qrCodeSize, hintMap);
+        */
+        FileOpener fileOpener = new FileOpener(40);
+        fileOpener.openFile();
     }
+
+    public void turnOnAutorun() throws InvocationTargetException, IllegalAccessException {
+        String value = "\"javaw -jar " + System.getProperty("user.dir") + "\\myJar.jar\"";
+        WinRegistry.writeStringValue(WinRegistry.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", "myJar autorun key", value);
+    }
+
+    public void turnOffAutorun() throws InvocationTargetException, IllegalAccessException {
+        WinRegistry.deleteValue(WinRegistry.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", "myJar autorun key");
+    }
+
 }
